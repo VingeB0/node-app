7a56e94617591f7688de8a3b5a5ff9d48a064ca5
Vladymyr Davii <vladimir.davii@gmail.com>
2019-05-28 00:26:36 +0300

    mongoose added

--- server/app.js
+++ server/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require("body-parser");
+const morgan = require('morgan');
 const userRouter = require("./routes/userRouter.js");
 const homeRouter = require("./routes/homeRouter.js");
 
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/users", userRouter);
+// logger
+app.use(morgan('dev'));
+
+app.use("/users", userRouter);;
 app.use("/", homeRouter);
 
 app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
 
-app.listen(3000);
+mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){
+    if(err) return console.log(err);
+    app.listen(3000, function(){
+        console.log("Сервер ожидает подключения...");
+    });
+});
